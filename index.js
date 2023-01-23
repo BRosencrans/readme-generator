06f3929f889ae081717ecb8d6670c00ba2cb54e6
BRosencrans <brandon.rosencrans@gmail.com>
2023-01-22 18:49:38 -0800

    turned questions into an array, moved inquirer to init function

--- index.js
+++ index.js
@@ -2,45 +2,52 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 // TODO: Create an array of questions for user input
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: "Please enter the title of your project",
-            name: 'title',
-         },
-         {
-            type: 'input',
-            message: "Please enter a description of your project",
-            name: 'description',
-         },
-         {
-            type: 'input',
-            message: "Please enter installation guide for your project(if needed)",
-            name: 'installation',
-         },
-         {
-            type: 'input',
-            message: "Please enter any usage information needed for your project",
-            name: 'usage',
-         },
-         {
-            type: 'input',
-            message: "Please enter any collaborators or thank yous here",
-            name: 'thanks',
-         },
-         {
-            type: 'input',
-            message: "Please enter any testing information here",
-            name: "tests",
-         }
-    ])
+const answers = [
+   {
+       type: 'input',
+       message: "Please enter the title of your project",
+       name: 'title',
+    },
+    {
+       type: 'input',
+       message: "Please enter a description of your project",
+       name: 'description',
+    },
+    {
+       type: 'input',
+       message: "Please enter installation guide for your project(if needed)",
+       name: 'installation',
+    },
+    {
+       type: 'input',
+       message: "Please enter any usage information needed for your project",
+       name: 'usage',
+    },
+    {
+       type: 'input',
+       message: "Please enter any collaborators or thank yous here",
+       name: 'thanks',
+    },
+    {
+       type: 'input',
+       message: "Please enter any testing information here",
+       name: "tests",
+    }
+]
+
+
 
 // TODO: Create a function to write README file
 //function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-//function init() {}
+function init() {inquirer
+   .prompt(answers).then(function(newAnswers){
+mrkDownData = newAnswers
+console.log(mrkDownData)
+//fs.write(mrkDownData)
+   })
+}
 
 // Function call to initialize app
-//init();
+init();
